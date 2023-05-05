3205f8c7e9d233accd054069df2c17fe34cbd40e
Saurav <Sauravchaudhary939@gmail.com>
2023-05-05 22:53:58 +0530

    react handson 3

---
+++ src/Compoment/abc.js
@@ -0,0 +1,58 @@
+/* body{
+  background-image: linear-gradient(0deg, #00ffe7, pink,transparent);
+  background-repeat:repeat-y;
+  background-size: cover;
+  height: 100vh;
+  overflow-y: hidden;
+}
+h1 {
+  text-align: center;
+}
+form {
+  text-align: center;
+}
+input {
+  border-radius: 15px;
+  width: 200px;
+  height: 25px;
+  margin-top: 15px;
+  background-color: lightblue;
+}
+button {
+  margin-top: 25px;
+  border-radius: 15px;
+  width: 110px;
+  height: 35px;
+  background-color: rgb(88, 135, 222);
+}
+
+.Value-Container {
+  display: flex;
+flex-wrap: wrap;
+justify-content:space-evenly;
+border: 2px solid black;
+
+background-color: aqua;
+
+position: absolute; top: 11.4% ;
+left : 3%;
+
+}
+hr {
+  background-color: black;
+}
+.Value-Box{
+  border: 2px solid black;
+  width: 400px; height: 65px;
+  background-color: palevioletred;
+  border-radius: 8px;
+  margin: 5px;
+}
+.Back-Button{
+  position: relative; top: 350px ; left: 600px;
+  padding: 10px;
+  border-radius: 5px;
+  width: 100px;
+  background-color:aqua;
+  cursor: pointer;
+} */
